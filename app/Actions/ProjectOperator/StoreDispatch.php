bf8d4f4ddc6937b57a17863842b84fc73cf6c93f
unknown <minority1123@gmail.com>
2026-03-02 12:11:31 +0900

    WIP_CSV_PROCEDURE

--- app/Actions/ProjectOperator/StoreDispatch.php
+++ app/Actions/ProjectOperator/StoreDispatch.php
@@ -4,71 +4,39 @@ namespace App\Actions\ProjectOperator;
 
 use App\Models\DistributionPlan;
 use App\Models\Project;
+use App\Support\ProjectOperator\DispatchCSVProcessor;
 
 // 営業所ごとに登録する
 class StoreDispatch{
 
     // 案件データを営業所に登録
     public static function send_projects_to_branch($request){
-        // ファイルの取得(配列で)
-        $files=$request->fileSets;
+        //営業所の取得
+        $place=$request->place;
+
         // CSVから案件名=>[town,start,end]の入れ子配列の取得
-         $project_name_and_towns=self::get_data_in_files($files);
-        // Projectモデルに案件データを入れる（併配含む）
-        self::store_projects_data($request);
+         $project_name_and_towns=DispatchCSVProcessor::get_data_in_files($request->fileSets);
+ 
+         // projectsに案件データを入れる（併配含む）
+        //  projectsは現在は必要ないが、外部キー等の関係上、入力しておき、確認などに使用する
+        self::store_projects_data($project_name_and_towns);
 
-        // DistributionRecordに案件Idと町目を入れる
-        self::store_distribution_data($request);
+        // distribution_plansに案件Idと町目を入れる
+        self::store_distribution_plan($place,$project_name_and_towns);
 
-    }
-
-    public static function get_data_in_files($files){
-        foreach($files as $file){
-            // メイン案件名
-            $main_project_name=$file->getClientOriginalName();
-            // ファイルにアクセス
-             $handle = fopen($file->getRealPath(), 'r');
-            // ファイルのの内容を見る(fgetsCSVが自動的に1行ずつ見てくれる)
-            $one_flug=false;
-            // サブ案件リスト
-            $sub_projects_lists=[];
-            while (($row = fgetcsv($handle)) !== false) {
-                if(!$one_flug){
-                    // １行目の場合
-
-                    $one_flug=true;
-                    return;
-                }
-
-                // $row[0], $row[1], $row[2] ...
-
-            }
-        }
-        return [];
-    }
-
-
-        // ファイル名とファイルの１〜２列目から、メイン案件の町名の配列取得
-     public static function get_main_project_sets(){
-            return[];
-     }
-
-    // ファイルの１行目の３列目以降と２行目以下の町名と○xから併配の配列を取得
-    public static function get_sub_projects_sets(){
-        return [];
     }
 
     // projectのsql挿入(日付と名前)
     // むしろProjectテーブルいらないかも！？
     // 今後にProjectの大元を管理することも含めておいておく
-    public static function store_projects_data(){
+    public static function store_projects_data($project_name_and_towns){
 
         $project=new Project();
 
     }
 
     // 配布データに町目だけ入れる
-    public static function store_distribution_data($request){
+    public static function store_distribution_plan($project_name_and_towns){
         $disribution_plan=new DistributionPlan();
 
     }
---
+++ app/Support/ProjectOperator/DispatchCSVProcessor.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Support\ProjectOperator;
+
+//案件を営業所に振る際におけるCSV処理
+class DispatchCSVProcessor{
+    // ファイルから取得する全体の流れ
+    public static function get_data_in_files($files){
+        foreach($files as $file){
+            // メイン案件名
+            $main_project_name=$file->getClientOriginalName();
+            // ファイルにアクセス
+             $handle = fopen($file->getRealPath(), 'r');
+            // ファイルのの内容を見る(fgetsCSVが自動的に1行ずつ見てくれる)
+            $one_flug=false;
+            // 返却する案件=>town,dateのリスト(内容違いの同じ案件が登録されるケースを考え、無条件での初期化はしない)
+            if(!isset($return_sets[$main_project_name])){
+                $return_sets[$main_project_name]=[];
+            }
+            // サブ案件リスト
+            $sub_projects_lists=[];
+            // 各案件リスト
+            while (($row = fgetcsv($handle)) !== false) {
+              $return_sets=self::each_file_procedure($one_flug,$row,$main_project_name,$sub_projects_lists,$return_sets);
+            }
+        }
+
+        //PHPのforeachはスコープを作らないのでこれでOK
+        return $return_sets;
+    }
+
+    // 各csvのファイル内部での処理
+    public static function each_file_procedure($one_flug,$row,$main_project_name,$sub_projects_lists,$return_sets){
+
+                if(!$one_flug){
+                    // １行目の場合、案件名を取得し保存する
+                    for($column=3;$column<count($row);$column++){
+                        $sub_projects_lists[]=$row[$column];
+                    }
+                    $one_flug=true;
+                    return;
+                }
+
+                // ２行目以下の場合
+                
+                // 期限
+                $start_date=$row[0];
+                $end_date=$row[1];
+                // 市町村(idで返す方が良い！)
+                $address=$row[2].$row[3];
+
+                // メイン案件リストに追加(同じ案件内でも併配によって期日が違う場合があるので、Dateも1つずつ行う)
+                $return_sets[$main_project_name][]=[
+                    "start_date"=>$start_date,
+                    "end_date"=>$end_date,
+                    "address"=>$address
+                ];
+
+                // 併配リストに「○」がついているとき、サブ案件リストに追加
+                for($column=3;$column<count($row);$column++){
+                    if(in_array($row[$column],["〇","○"])){
+                    $return_sets[$row[$column]][]=[
+                        "start_date"=>$start_date,
+                        "end_date"=>$end_date,
+                        "address"=>$address
+                     ];
+                    }
+                }
+
+        return $return_sets;
+
+    }
+
+}
